a7a146780c983c55eb7367fdd4919e4ff9c03db4
Kris Jenkins <krisajenkins@gmail.com>
2015-11-16 20:46:12 +0000

    Tidying.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,9 +2,11 @@ module Lib
     ( someFunc
     ) where
 
+import           Control.Monad
 import           Data.Char
-import           Data.Map  (Map)
-import qualified Data.Map  as Map
+import           Data.Map      (Map)
+import qualified Data.Map      as Map
+import           Data.Maybe
 
 data Node
   = Empty
@@ -13,27 +15,23 @@ data Node
 
 ------------------------------------------------------------
 
-addPhrase :: String -> Node -> Node
-addPhrase [] n = n
-addPhrase (c:cs) Empty =
+addPhrase :: Node -> String -> Node
+addPhrase n [] = n
+addPhrase Empty (c:cs) =
   Edge (Map.singleton c
-                      (addPhrase cs Empty))
-addPhrase (c:cs) (Edge m) =
+                      (addPhrase Empty cs))
+addPhrase (Edge m) (c:cs) =
   Edge $
-  Map.insert
-    c
-    (addPhrase cs
-               (case Map.lookup c m of
-                  Nothing -> Empty
-                  Just subnode -> subnode))
-    m
+  Map.insert c
+             (addPhrase (fromMaybe Empty (Map.lookup c m)) cs)
+             m
 
 readSubtree :: (Char,Node) -> [String]
 readSubtree (k,v) = fmap (k:) (readPhrases v [])
 
 readPhrases :: Node -> String -> [String]
 readPhrases Empty _ = [""]
-readPhrases (Edge m) [] = concat $ fmap readSubtree (Map.assocs m)
+readPhrases (Edge m) [] = concatMap readSubtree (Map.assocs m)
 readPhrases (Edge m) (c:cs) =
   case Map.lookup c m of
     Just subnode -> fmap (c :) (readPhrases subnode cs)
@@ -42,7 +40,7 @@ readPhrases (Edge m) (c:cs) =
 ------------------------------------------------------------
 
 a :: Node
-a = addPhrase "bar" $ addPhrase "baz" $ addPhrase "foo" Empty
+a = foldl addPhrase Empty ["bar", "baz", "foo"]
 
 b :: [String]
 b = readPhrases a "q"
@@ -52,13 +50,19 @@ b = readPhrases a "q"
 readShakespeare :: FilePath -> IO [String]
 readShakespeare file =
   do corpus <- readFile file
-     return $ fmap (dropWhile isSpace) $ lines corpus
+     return $ dropWhile isSpace <$> lines corpus
+
+repl :: Node -> IO ()
+repl trie =
+  do putStrLn "Query: "
+     prefix <- getLine
+     mapM_ putStrLn (take 20 (readPhrases trie prefix))
+     putStrLn "-------"
+     unless (null prefix)
+            (repl trie)
 
 someFunc :: IO ()
 someFunc =
   do shakespeare <- readShakespeare "Shakespeare_Complete_Works.txt"
-     let trie =
-           foldl (\acc line -> addPhrase line acc)
-                 Empty
-                 shakespeare
-     mapM_ print (take 20 (readPhrases trie "CLOWN"))
+     let trie = foldl addPhrase Empty shakespeare
+     repl trie
